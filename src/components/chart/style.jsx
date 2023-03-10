d19d7c75748c649931231c339c274e07962713f5
Gabriel Azevedo <gbldja@gmail.com>
2023-03-10 01:17:34 -0300

    chore: import GlobalStyle on main

---
+++ src/components/chart/style.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const ContentChart = styled.div`
+  height: auto;
+  width: 50%;
+  padding: 1rem;
+  background-color: #fff;
+  border-radius: 1rem;
+
+box-shadow: -.2rem .1rem 2rem #9d9b9b;
+`
---
+++ src/components/header/index.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const ContainerHeader = styled.header`
+  width: 100%;
+  height: 5rem;
+  background-color: #a9a9a9;
+
+  position: fixed;
+  top: 0;
+`
+
+export default function Header() {
+  return (
+    <>
+      <ContainerHeader>
+        
+      </ContainerHeader>
+    </>
+  )
+}
