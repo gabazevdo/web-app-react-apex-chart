9ff11a2c370a9d28f0e64bab82e517d63399abfa
Gabriel Azevedo <gbldja@gmail.com>
2023-03-10 01:17:01 -0300

    chore: create directory page and files Home and style

---
+++ src/pages/style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ContainerHome = styled.main`
+  height: 100vh;
+  width: 100%;
+
+  background-color: #F4F4F8;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
